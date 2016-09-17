455f9340ba74220d768d54fc64558a3fba57ba52
kolosa <limoyan020@163.com>
2016-09-17 21:47:48 +0800

    Add files via upload

--- task18/task18.js
+++ task18/task18.js
@@ -1,13 +1,21 @@
-var arr=[1,2,3];
+var arr=[];
 function leftenter(){
     var val=document.getElementById("area").value;
-    arr.unshift(val);
-    alert(arr);
+    if ((/^[0-9]+$/).test(val)){
+        arr.unshift(val);
+        alert(arr);}
+    else {
+        alert("请输入数字");
+    }
 }
 function rightenter() {
     var val=document.getElementById("area").value;
-    arr.push(val);
-    alert(arr);
+    if ((/^[0-9]+$/).test(val)){
+        arr.push(val);
+        alert(arr);}
+    else {
+        alert("请输入数字");
+    }
 }
 function leftdelete(){
     var arry=arr.splice(0,1);
@@ -15,8 +23,12 @@ function leftdelete(){
 }
 function rightdelete(){
  var i=arr.length-1;
-    var arry=arr.splice(i,1)
-    alert(arr)
+    var arry=arr.splice(i,1);
+    alert(arr);
 }
 
-
+    for(var i=0;i<arr.length;i++){
+        var blo=document.getElementsById("block");
+        var ar=arr[i].value;
+        bol.append(ar);
+    }
